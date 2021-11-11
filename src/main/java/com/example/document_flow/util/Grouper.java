aaac4d23bfe10e063843fe24a78c5b5034b1be6c
Rustic2 <rus5i1819@gmail.com>
2021-11-11 23:54:18 +0500

    0.9(add JavaDoc Grouper.java)

--- src/main/java/com/example/document_flow/util/Grouper.java
+++ src/main/java/com/example/document_flow/util/Grouper.java
@@ -12,6 +12,14 @@ public class Grouper {
 
     private Map<Person, List<Document>> documentByAuthor = new HashMap<>();
 
+    /**
+     * Группирует переданный массив документов по автору
+     *
+     * @param documentList список документов
+     * @return Map'у Документов сгруппированный по авторам.
+     * Где ключ <code>Person</code> это автор.
+     * Значение это список документов, созданный им.
+     */
     public Map<Person, List<Document>> groupByAuthor(ArrayList<Document> documentList) {
         for (Document d : documentList) {
             if (documentByAuthor.containsKey(d.getAuthor())) {
@@ -24,6 +32,18 @@ public class Grouper {
         return documentByAuthor;
     }
 
+    /**
+     *
+     * @return Отчет перечень авторов документов и список документов, созданных ими
+     *<pre>
+     *Андреева Елизавета Павловна:
+     *  * Входящее №15 от 3 марта 2017 Имя документа : Первый документ
+     *  * Поручение №27 от 26 января 2017 Имя документа : Первый документ
+     *Орлов Глеб Михайлович:
+     *  * Поручение №19 от 24 января 2019 Имя документа : Третий документ
+     *  * Поручение №83 от 12 февраля 2021 Имя документа : Второй документ
+     *</pre>
+     */
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();

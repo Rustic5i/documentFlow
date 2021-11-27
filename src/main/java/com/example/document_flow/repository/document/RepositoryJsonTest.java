e3f27ccbfc3ce834f95be82543bf695488e60808
Rustic2 <rus5i1819@gmail.com>
2021-11-27 23:51:30 +0500

    PROGPER-5023 ()

--- src/main/java/com/example/document_flow/repository/document/RepositoryJsonTest.java
+++ src/main/java/com/example/document_flow/repository/document/RepositoryJsonTest.java
@@ -1,16 +1,14 @@
 package com.example.document_flow.repository.document;
 
 import com.example.document_flow.entity.document.Document;
+import com.example.document_flow.entity.staff.Person;
 import com.example.document_flow.repository.InMemory;
 import com.example.document_flow.repository.abstraction.DAO.DAO;
 import com.example.document_flow.util.read.DeserializationJSON;
 import com.example.document_flow.util.write.SerializableJSON;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RepositoryJsonTest implements DAO<Document> {
 
@@ -25,17 +23,22 @@ public class RepositoryJsonTest implements DAO<Document> {
     @Override
     public void save(Document object) {
         String filePath = object.getAuthor().toString() + ".json";
-        serializable.toJson(filePath,object);
+        serializable.toJson(filePath, object);
         setPathFilJson.add(filePath);
         inMemory.save(filePath, Arrays.asList(object));
     }
 
     @Override
-    public void saveAll(List<Document> objects) {  ///Отсановился тут. Тут баг !!!
-        String filePath = objects.get(0).getAuthor().toString() + ".json";
-        serializable.toJson(filePath,objects);
-        setPathFilJson.add(filePath);
-        inMemory.save(filePath,objects);
+    public void saveAll(List<Document> objects) {
+        Map<Person, List<Document>> documentByAuthor = objects.stream().collect(Collectors.groupingBy(Document::getAuthor));
+        StringBuilder author = new StringBuilder();
+        documentByAuthor.entrySet().stream().forEach(value -> {
+            author.append(value.getKey()).append(".json");
+            serializable.toJson(author.toString(), value.getValue());
+            setPathFilJson.add(author.toString());
+            inMemory.save(author.toString(), value.getValue());
+            author.delete(0, author.length());
+        });
     }
 
     @Override

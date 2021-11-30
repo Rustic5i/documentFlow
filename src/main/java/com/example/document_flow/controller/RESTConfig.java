58e889de73cdd043847ddd8c8bd4281d2e098e51
Баратов Руслан <rbaratov@citros.ru>
2021-11-30 20:15:18 +0500

    PROGPER-5024-REST-3()

--- src/main/java/com/example/document_flow/controller/RESTConfig.java
+++ src/main/java/com/example/document_flow/controller/RESTConfig.java
@@ -3,7 +3,6 @@ package com.example.document_flow.controller;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-
 @ApplicationPath("/ecm")
 class RESTConfig extends Application {
 }

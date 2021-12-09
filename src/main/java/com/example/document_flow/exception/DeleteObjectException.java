bd713729da6d5d18364f5657321249745b6d81f9
Баратов Руслан <rbaratov@citros.ru>
2021-12-09 20:09:15 +0500

    PROGPER-5025-4(javaDoc)

--- src/main/java/com/example/document_flow/exception/DeleteObjectException.java
+++ src/main/java/com/example/document_flow/exception/DeleteObjectException.java
@@ -1,6 +1,13 @@
 package com.example.document_flow.exception;
 
+/**
+ * В случае, когда удаление объекта терпит неудачу по какой-либо причине,
+ * то необходимо выбрасывать исключение.
+ *
+ * @author Баратов Руслан
+ */
 public class DeleteObjectException extends Throwable {
+
     public DeleteObjectException(String message) {
         super(message);
     }
